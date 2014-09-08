8dfc0697b309ae428d1b386d8002cf51e00c301c
jblowe <jblowe@berkeley.edu>
2014-09-07 21:46:16 -0700

    NOJIRA: update landing page age and wsgi

--- landing/views.py
+++ landing/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.conf import settings
 
+from cspace_django_site.main import cspace_django_site
+from common import cspace
+
+config = cspace_django_site.getConfig()
+hostname = cspace.getConfigOptionWithSection(config,
+                                             cspace.CONFIGSECTION_AUTHN_CONNECT,
+                                             cspace.CSPACE_HOSTNAME_PROPERTY)
 
 TITLE = 'Applications Available'
 
@@ -11,6 +18,7 @@ hiddenApps = 'hello service suggest suggestsolr suggestpostgres solarapi imagese
 
 #@login_required()
 def index(request):
+    config = cspace_django_site.getConfig()
     appList = [app for app in settings.INSTALLED_APPS if not "django" in app and not app in hiddenApps]
     appList.sort()
-    return render(request, 'listApps.html', {'appList': appList, 'labels': 'name file'.split(' '), 'title': TITLE})
+    return render(request, 'listApps.html', {'appList': appList, 'labels': 'name file'.split(' '), 'title': TITLE, 'hostname': hostname})
