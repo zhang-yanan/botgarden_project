5bd4a9fbc029380bf519ea0f02bb744686abcd1b
jblowe <jblowe@berkeley.edu>
2014-09-07 20:31:40 -0700

    NOJIRA: fix filename capitalization problem, text in urls.

--- search/urls.py
+++ search/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 from search import views
 
 urlpatterns = patterns('',
-                       url(r'^search/$', views.search, name='publicSearch'),
+                       url(r'^search/$', views.search, name='search'),
                        url(r'^results/$', views.retrieveResults, name='retrieveResults'),
                        url(r'^bmapper/$', views.bmapper, name='bmapper'),
                        url(r'^csv/$', views.csv, name='csv'),
